7d96d26aea40f74e1329593bb471b913c17b7d04
Ivanko-Mosiychuk <MWIvanko@gmail.com>
2020-09-19 17:31:29 +0300

    Add all get & set to Item.class

--- src/main/java/logic/Item.java
+++ src/main/java/logic/Item.java
@@ -10,4 +10,28 @@ public class Item {
         this.available = available;
         this.remainder = remainder;
     }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getRemainder() {
+        return remainder;
+    }
+
+    public void setRemainder(int remainder) {
+        this.remainder = remainder;
+    }
 }
